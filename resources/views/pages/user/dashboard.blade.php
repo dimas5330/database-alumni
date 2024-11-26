914150f28c316d2fb6b337a6cce255ad005cbd13
SamuelAdityas <adit.gf.17@gmail.com>
2024-11-26 20:13:27 +0700

    Add DataPribadi form and controller for user data submission

--- resources/views/pages/user/dashboard.blade.php
+++ resources/views/pages/user/dashboard.blade.php
@@ -23,7 +23,7 @@
                         <div class="card-body">
                             <div class="form-group">
                                 <label>Nama Lengkap</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="Richard Adi Setianto">
                             </div>
                             <div class="section-title mt-0">Jenis Kelamin</div>
                             <div class="form-group">
@@ -34,11 +34,15 @@
                             </div>
                             <div class="form-group">
                                 <label>Tempat Lahir</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="Semarang">
                             </div>
                             <div class="form-group">
                                 <label>Tanggal Lahir</label>
                                 <input type="text" class="form-control datemask" placeholder="YYYY/MM/DD">
+                            </div>
+                            <div class="form-group">
+                                <label>Tanggal Lahir 2</label>
+                                <input type="date" id="date" name="date" required>
                             </div>
                             <div class="form-group">
                                 <label class="form-label">Golongan Darah</label>
@@ -70,36 +74,42 @@
                                             <i class="fas fa-phone"></i>
                                         </div>
                                     </div>
-                                    <input type="text" class="form-control phone-number">
+                                    <input type="text" class="form-control phone-number" placeholder="085743156853">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label>Alamat Tempat Tinggal</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="Jln. Raden Sambung No. 45">
                             </div>
                             <div class="form-group">
                                 <label>Email</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="example@gmail.com">
                             </div>
                             <div class="form-group">
                                 <label>Angkatan Lulus SMA</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="2015">
                             </div>
                             <div class="form-group">
                                 <label>Nama Sekolah/Kampus</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="SMAN 1 Semarang">
                             </div>
                             <div class="form-group">
                                 <label>Pekerjaan</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="Perawat">
                             </div>
                             <div class="form-group">
                                 <label>Nama Kantor</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="PT. Abdi Sentosa">
                             </div>
                             <div class="form-group">
                                 <label>Alamat Kantor</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" placeholder="Jln. Singkong No.86">
+                            </div>
+                            <div class="form-group row mb-4">
+                                <label class="col-12 mb-4"></label>
+                                <div class="col-sm-12 col-md-7">
+                                    <button class="btn btn-primary">Publish</button>
+                                </div>
                             </div>
                     </div>
                 </div>
