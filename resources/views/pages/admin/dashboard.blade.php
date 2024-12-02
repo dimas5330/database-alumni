a933095e18317d5519a5b6385055502b5f269b73
dimas5330 <goenardid@gmail.com>
2024-12-02 11:31:27 +0700

    Update dashboard layout: simplify column classes for better responsiveness

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -79,7 +79,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-8 col-md-12 col-12 col-sm-12">
+                <div class="col-12 mb-4">
                     <div class="card">
                         <div class="card-header">
                             <h4>Statistics</h4>
