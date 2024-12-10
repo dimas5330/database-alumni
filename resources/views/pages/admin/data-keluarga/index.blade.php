8ac8cfd81e1eaeff4ba6bfbf9129fed82ee3b69f
SamuelAdityas <adit.gf.17@gmail.com>
2024-12-10 15:23:01 +0700

    Add edit and delete buttons for Data Keluarga in admin view

--- resources/views/pages/admin/data-keluarga/index.blade.php
+++ resources/views/pages/admin/data-keluarga/index.blade.php
@@ -83,6 +83,25 @@
                                                 <td>
                                                     {{ $data->nama_anak }}
                                                 </td>
+                                                <td>
+                                                    <div class="d-flex justify-content-center">
+                                                        <a href='{{ route('dataKeluarga.edit', $data->id) }}'
+                                                            class="btn btn-sm btn-info btn-icon">
+                                                            <i class="fas fa-edit"></i>
+                                                            Edit
+                                                        </a>
+
+                                                        <form action="{{ route('dataKeluarga.destroy', $data->id) }}"
+                                                            method="POST" class="ml-2">
+                                                            <input type="hidden" name="_method" value="DELETE" />
+                                                            <input type="hidden" name="_token"
+                                                                value="{{ csrf_token() }}" />
+                                                            <button class="btn btn-sm btn-danger btn-icon confirm-delete">
+                                                                <i class="fas fa-times"></i> Delete
+                                                            </button>
+                                                        </form>
+                                                    </div>
+                                                </td>
                                             </tr>
                                         @endforeach
 
