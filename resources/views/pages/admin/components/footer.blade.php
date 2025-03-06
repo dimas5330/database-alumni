486286d30cec58d9c2762f66a3521b7414946a9c
dimas5330 <goenardid@gmail.com>
2025-03-06 14:38:43 +0700

    Update footer copyright and change application title to 'Alumni Perkantas Semarang'; add new favicon

--- resources/views/pages/admin/components/footer.blade.php
+++ resources/views/pages/admin/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2024 <div class="bullet"></div> Dibuat Oleh IT Perkantas</a>
+        Copyright &copy; 2024 <div class="bullet"></div> Dibuat Oleh Perkantas Semarang</a>
     </div>
     <div class="footer-right">
         1.0.0
